fb1f6917dad2889d232e944761999e311ef25845
samuel <samuel.laestander@edu.lycksele.se>
2025-01-31 14:03:26 +0100

    first commit

---
+++ WebSockets testing/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WebSocketSharp;
+
+
+
+class Program{
+
+  static void Main(string[] args){
+
+    // create a instance of a websocket client
+    WebSocket ws = new WebSocket("ws://simple-websocket-server-echo.glitch.me/");
+
+
+    ws.Connect();
+    ws.Send("Hello server");
+
+    Console.ReadKey();
+  }
+}
